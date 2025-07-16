6339ad7eb060225cc3a9f7391aeb49364c5c4cc9
Douglas O. Luciano <douglas.luciano2@gmail.com>
2025-07-15 23:43:12 -0300

    Resolving conflict on import.sql manually

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,10 +2,10 @@ INSERT INTO banks (code, name) VALUES
   ('000', 'Banco Vazio'),
   ('999', 'Banco Cheio');
 
-INSERT INTO accounts (name, account_type, bank_id, branch, account_number, credit_limit) VALUES
-  ('Conta Corrente Exemplo', 'CHECKING', 1, '0001', '0001', 1000000),
-  ('Conta Poupança Exemplo', 'SAVINGS' , 1, '0001', '0002', 0);
-
 INSERT INTO tags (name, color) VALUES
   ('Tag existe 1', 1234),
   ('Tag existe 2', 3216);
+
+INSERT INTO accounts (name, account_type, bank_id, branch, account_number, credit_limit) VALUES
+  ('Conta Corrente Exemplo', 'CHECKING', 1, '0001', '0001', 1000000),
+  ('Conta Poupança Exemplo', 'SAVINGS' , 1, '0001', '0002', 0);
